8591ff1b07e53317e07e504c041d98d0fedaf059
Egopr <93969256+Egopr@users.noreply.github.com>
2024-06-05 17:23:25 +0300

    Add files via upload

---
+++ LTM8054_Serial.ino
@@ -0,0 +1,31 @@
+int lavr;
+void setup() {
+  
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  digitalWrite(2, LOW);
+  pinMode(3, OUTPUT);
+
+}
+
+void loop() {
+
+  
+ if (Serial.available() > 0) 
+ {
+    lavr = Serial.parseInt();
+    switch (lavr) {
+    case 1:
+      digitalWrite(2, LOW);
+      digitalWrite(3, HIGH);
+      Serial.println("Reley ON");
+      break;
+    case 2:
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+      Serial.println("Relay    OFF");
+      break;
+  }
+
+}
+}
